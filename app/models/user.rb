056349fa603089219a1979ef8943122f89d69d2f
Joey Bachand <bachand.joey@gmail.com>
2014-05-05 15:54:46 -0500

    Engineering Open House Course

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
 	# has_many :courses, through: :messages
 	before_save {self.email = email.downcase}
 	before_create :create_remember_token
+	before_create :set_image
+	after_create :set_course
 	validates :first_name, presence: true, length: { maximum: 50}
 	validates :last_name, presence: true, length: { maximum: 50}
 	# wichita.edu REGEX  /\A[\w+\-.]+@wichita.edu
@@ -28,6 +30,15 @@ class User < ActiveRecord::Base
 	end
 
 	private
+	def set_image
+		self.image = 'wu_shock.jpg'
+	end
+
+	def set_course
+		course = Course.find_by(crn: "00000")
+		@assignment = self.assignments.new(user_id: self.id, course_id: course.id)
+		@assignment.save
+	end
 
 	def create_remember_token
 	 	self.remember_token = User.digest(User.new_remember_token)
